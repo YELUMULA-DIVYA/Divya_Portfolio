981e2d6bf1e6ca40b3eb9847361d74a3212156f8
YELUMULA-DIVYA <yelumuladivya3@gmail.com>
2026-06-14 16:14:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,21 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
             });
     });
 });
+
+const observer = new IntersectionObserver((entries)=>{
+    entries.forEach(entry=>{
+        if(entry.isIntersecting){
+            entry.target.classList.add("show");
+        }
+    });
+});
+
+document.querySelectorAll("section").forEach((el)=>{
+    el.classList.add("hidden");
+    observer.observe(el);
+});
+
+document.addEventListener("mousemove",(e)=>{
+document.body.style.setProperty("--x",e.clientX+"px");
+document.body.style.setProperty("--y",e.clientY+"px");
+});
